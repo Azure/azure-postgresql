61e1739d3e7566f68bd92da90f37d7863eaca90e
Sunitha Muthukrishna <sumuth@microsoft.com>
2020-12-10 08:40:14 -0800

    Update settings.py

--- samples/AKS-Demos/Django-Demo/Code/django_postgres_app/settings.py
+++ samples/AKS-Demos/Django-Demo/Code/django_postgres_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '25cd8ba5-dffc-4873-a4ff-bbcca2d37a57'
+SECRET_KEY = os.getenv('APP_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
